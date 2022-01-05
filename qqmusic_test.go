0832e239cdd6f09a0d0699d3db4d77fafaca3812
Wendell Sun <iwendellsun@gmail.com>
2022-01-06 01:46:59 +0800

    fix test case

--- qqmusic_test.go
+++ qqmusic_test.go
@@ -1,39 +1,64 @@
 package qqmusic
 
 import (
+	"os"
 	"testing"
 )
 
+func getCookie() string {
+	return os.Getenv("QQMUSIC_COOKIE")
+}
+
 func TestSearchSinger(t *testing.T) {
-	singerInfo, err := New().SearchSinger("美波")
+	cookie := getCookie()
+	if cookie == "" {
+		t.Skip()
+	}
+	singerInfo, err := New(cookie).SearchSinger("美波")
 	if err != nil || singerInfo == nil {
 		t.Error(err, singerInfo)
 	}
 }
 
 func TestGetSingerFansNum(t *testing.T) {
-	count, err := New().GetSingerFansCount("003fA5G40k6hKc")
+	cookie := getCookie()
+	if cookie == "" {
+		t.Skip()
+	}
+	count, err := New(cookie).GetSingerFansCount("003fA5G40k6hKc")
 	if err != nil || count == 0 {
 		t.Error(err, count)
 	}
 }
 
 func TestGetSonglistBySinger(t *testing.T) {
-	totalNum, songInfos, err := New().GetSonglistBySinger("003fA5G40k6hKc", 1, 2)
+	cookie := getCookie()
+	if cookie == "" {
+		t.Skip()
+	}
+	totalNum, songInfos, err := New(cookie).GetSonglistBySinger("003fA5G40k6hKc", 1, 2)
 	if err != nil || len(songInfos) == 0 || totalNum == 0 {
 		t.Error(err, songInfos, totalNum)
 	}
 }
 
 func TestGetSongCommentCount(t *testing.T) {
-	countMap, err := New().GetSongCommentCount([]int{311084300, 203650936})
+	cookie := getCookie()
+	if cookie == "" {
+		t.Skip()
+	}
+	countMap, err := New(cookie).GetSongCommentCount([]int{311084300, 203650936})
 	if err != nil || len(countMap) == 0 {
 		t.Error(err, countMap)
 	}
 }
 
 func TestGetSongFavorCount(t *testing.T) {
-	countMap, err := New().GetSongFavorCount([]string{"004Q9vXu2FuqDs", "0045YdtG4FSRLN"})
+	cookie := getCookie()
+	if cookie == "" {
+		t.Skip()
+	}
+	countMap, err := New(cookie).GetSongFavorCount([]string{"004Q9vXu2FuqDs", "0045YdtG4FSRLN"})
 	if err != nil || len(countMap) == 0 {
 		t.Error(err, countMap)
 	}
